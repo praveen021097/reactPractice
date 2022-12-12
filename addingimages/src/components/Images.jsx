dcda3385d0be30c9d07a49daf58044152ea538e8
praveen021097 <96250697+praveen021097@users.noreply.github.com>
2022-12-12 17:18:02 +0530

    second commit

---
+++ addingimages/src/components/Images.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import pic from "../images/pic.jpg";
+
+const Images=()=>{
+    return <div>
+    <img src={pic} alt="pk" />
+    <img src="https://images.freeimages.com/vhq/images/previews/43c/leather-stamping-logo-mockup-2-397107.jpg" alt="opensea" />
+    </div>
+}
+
+export default Images
