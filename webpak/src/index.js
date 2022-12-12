48de43bd1a8aecf85361e64f2b939517c33d464d
praveen021097 <96250697+praveen021097@users.noreply.github.com>
2022-12-12 22:40:15 +0530

    webpack-react

--- webpak/src/index.js
+++ webpak/src/index.js
@@ -1,11 +1,17 @@
 import {sum ,mul} from "./calc";
 import "./index.css"
+import React from "react";
+import reactDom from "react-dom";
 console.log(sum(5,5))
 console.log(mul(4,5))
 
-let root = document.getElementById("root");
 
-let h1 = document.createElement("h1");
-h1.innerText="hello world";
-h1.classList.add("redText");
-root.appendChild(h1);
+
+const h1=React.createElement("h1",{"className":"blacktext"},"hello Pk");
+
+reactDom.render(h1,document.getElementById("root"));
+
+// let h1 = document.createElement("h1");
+// h1.innerText="hello world";
+// h1.classList.add("redText");
+// root.appendChild(h1);
